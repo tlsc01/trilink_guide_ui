c5e59d5108bc9f1031f814f11eb838c28bde4ef8
Mike O'Dell <mikeodell77@gmail.com>
2018-05-08 15:10:37 -0400

    Add routing and home depot home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+// import React, { Component } from 'react';
+
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import './App.css';
-import LandingPage from './landingPage'
+import Router from './router';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
-ReactDOM.render(<LandingPage />, document.getElementById('root'));
+render((
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>
+), document.getElementById('root'));
 registerServiceWorker();
