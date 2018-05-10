dff6e0442f5769966b02400d1d6c60e5d8618bdc
Mike O'Dell <mikeodell77@gmail.com>
2018-05-09 20:19:29 -0400

    Fix routes for production

--- src/index.js
+++ src/index.js
@@ -4,16 +4,31 @@ import React from 'react';
 
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
 import './index.css';
 import './App.css';
-import Router from './router';
+// import Router from './router';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import LandingPage from './landingPage';
+import HomeDepot from './homeDepot';
+
+const routes = [
+  { path: '/',
+    exact: true,
+    component: LandingPage
+  },
+  { path: '/home-depot-fitup',
+    exact: true,
+    component: HomeDepot
+  }
+]
 
 render((
   <BrowserRouter>
-    <Router />
+    {/* <Router /> */}
+    {renderRoutes(routes)}
   </BrowserRouter>
 ), document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
--- src/router.js
+++ src/router.js
@@ -3,13 +3,30 @@ import { Switch, Route } from 'react-router-dom';
 import LandingPage from './landingPage';
 import HomeDepot from './homeDepot';
 
+const routes = [
+  { 
+    routes: [
+      { path: '/',
+        exact: true,
+        component: LandingPage
+      },
+      { path: '/home-depot-fitup',
+        component: HomeDepot
+      }
+    ]
+  }
+]
+
 const Router = () => (
-  <main>
-    <Switch>
-      <Route exact path='/' component={LandingPage}/>
-      <Route exact path='/home-depot-fitup' component={HomeDepot}/>
-    </Switch>
-  </main>
+  // <main>
+  //   <Switch>
+  //     <Route exact path='/' component={LandingPage}/>
+  //     <Route exact path='/home-depot-fitup' component={HomeDepot}/>
+  //   </Switch>
+  // </main>
+  <Route
+    location=
+  />
 )
 
 export default Router
